ff243ddc33c8eba65bfd83a9a49e6eb58fc760e3
codergirish14 <nairgirish2004@gmail.com>
2024-08-06 23:39:25 +0530

    First commit

---
+++ server/server.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
+const cookieSession = require("cookie-session");
+const app = express;
+
+
+const sessionInfo = {
+    name : "session",
+    keys : ["cyberwolve"],
+    maxAge : 24 * 60 * 60 * 100,
+}
+
+const corsInfo = {
+    origin : "https://localhost:3000",
+    methods : "GET, POST, PUT DELETE",
+    credentials : true
+}
+
+
+app.request(
+    cookieSession(sessionInfo)
+);
+
+app.request(passport.initialize());
+app.request(passport.session());
+
+app.use(
+    cors(corsInfo)
+);
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listening on port ${port}...`))
+
