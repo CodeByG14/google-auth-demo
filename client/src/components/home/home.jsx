4e2ddb77418705dee21f0835af9a778fad41c6e4
CodeByG14 <nairgirish2004@gmail.com>
2024-08-11 20:17:59 +0530

    Added some css

--- client/src/components/home/home.jsx
+++ client/src/components/home/home.jsx
@@ -1,35 +1,54 @@
-import React from 'react';
+import { useState, useEffect } from "react";
 import profile_img from "../../images/profile.jpg";
 import "./home.css";
+import PacmanLoader from "react-spinners/PacmanLoader";
 
 function Home(userDetails) {
-    const user = userDetails.user;
-    console.log(user)
-    const logout = () => {
-      window.open(
-        `${process.env.REACT_APP_API_URL}/auth/logout`,
-        "_self"
-      );
-    };
+  const user = userDetails.user;
+  const [loading, setLoading] = useState(true);
+  console.log(user)
+  const logout = () => {
+    window.open(
+      `${process.env.REACT_APP_API_URL}/auth/logout`,
+      "_self"
+    );
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false); // Set loading to false when data is ready
+    }, 2000);
+
+    return () => clearTimeout(timer); // Clean up timer on unmount
+  }, []);
 
   return (
-    <div className='container'>
-      <h1 className='heading'>Home</h1>
+    <>
+      {loading ? (
+      <PacmanLoader color="#ffc801" />
+      ) :(
+      <div className='container'>
+        <h1 className='heading'>Home</h1>
 
-      <div className='form_container'>
-        <div className='left'>
-          <img src={profile_img} alt='profile' />
-        </div>
+        <div className='form_container'>
+          <div className='left'>
+            <img src={profile_img} alt='profile' />
+          </div>
 
-        <div className='right'>
-          <h2 className='form_heading'>Profile</h2>
-          <img className='home_img' src={user.picture} alt='profile' />
-          <input className='input' type='text' defaultValue={user.name}></input>
-          <input className='input' type='text' defaultValue={user.email}></input>
-          <button className='btn'onClick={logout}>Log out</button>
+          <div className='right'>
+            <h2 className='form_heading'>Profile</h2>
+            <img className='home_img' src={user.picture} alt='profile' />
+            <div className="details">
+              <h2>{user.name}</h2>
+              <h2>{user.email}</h2>
+            </div>
+            <button className='btn' onClick={logout}>Log out</button>
+          </div>
         </div>
       </div>
-    </div>
+      )}
+
+    </>
   );
 }
 
--- client/src/components/login_page/login.jsx
+++ client/src/components/login_page/login.jsx
@@ -5,7 +5,7 @@ import google_img from "../../images/google.png"
 import "./login.css"
 
 function Login() {
-  const googleAuth = ()=>{
+  const googleAuth = () => {
     window.open(
       `${process.env.REACT_APP_API_URL}/auth/google/callback`,
       "_self"
@@ -19,22 +19,25 @@ function Login() {
 
       <div className='form_container'>
         <div className='left'>
-            <img src={login_img} alt='login'/>
+          <img src={login_img} alt='login' />
         </div>
-            
+
         <div className='right'>
-            <h2 className='from_heading'>Members Log in</h2>
-            <input type='email' className='input' placeholder='Email'/>
-            <input type='password' className='input' placeholder='Password'/> 
-            <button className='btn'>Log in</button>
-            <p className='text'>or</p>
-            <button className='google_btn' onClick={googleAuth}>
-              <img src={google_img} alt='google_icon'/>
-              <span>Sign in with Google</span>
-            </button>
-            <p className='text'>
-              New here ? <Link to="/signup">Sign Up</Link>
+          <h2 className='from_heading'>Members Log in</h2>
+          <input type='email' className='input' placeholder='Email' />
+          <input type='password' className='input' placeholder='Password' />
+          <button className='btn'>Log in</button>
+          <p className='text'>or</p>
+          <button className='google_btn' onClick={googleAuth}>
+            <img src={google_img} alt='google_icon' />
+            <span>Sign in with Google</span>
+          </button>
+          <div className="signup-container">
+            <p>New here?</p>
+            <p className='signup'>
+                <Link to="/signup">Sign Up</Link>
             </p>
+        </div>
         </div>
       </div>
     </div>
